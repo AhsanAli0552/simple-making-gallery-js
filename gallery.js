1ba0df0ed27bc2e80be0abf7b27cbeb6641379a6
Ahsan Ali <ahsanali71728@gmail.com>
2024-10-03 11:37:11 +0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,9 @@
+const imgbox = document.querySelector("#imgbox");
+const img = document.querySelectorAll("gallery-img img")
+
+// function ahsanali(small){
+//    imgbox.src = small.src;
+
+const ahsanali =(small) =>{
+  imgbox.src = small.src;
+} 
